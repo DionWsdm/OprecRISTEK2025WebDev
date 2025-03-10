ae29f76c0cd70a980f3558487b4ec97fc9adbf04
DionWsdm <dionwisdom29@gmail.com>
2025-03-10 12:52:23 +0700

    Main

---
+++ tryout-app-FE/src/main.tsx
@@ -0,0 +1,19 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './App.css'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './routes/Router.tsx'
+
+const root = createRoot(document.getElementById("root") as HTMLElement)
+
+root.render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+)
+
+// createRoot(document.getElementById('root')!).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
